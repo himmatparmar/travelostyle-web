ed2cda8e5614be0eaf1b4fb48780161fc5734270
alka-saiesha <alka.kumari@saiesha.us>
2026-06-08 11:36:01 +0530

    Defined root

---
+++ app/about-us/page.tsx
@@ -0,0 +1,5 @@
+export default function AboutUs() {
+    return(
+         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans bg-[#fafafa] w-[96vw] h-[54.15vw]">about us</div>
+    )
+}
