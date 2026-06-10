44e0692701fef2f24dece960dda097d443b5dfc0
mrsharmasumit33 <0706sumitsharma@gmail.com>
2026-06-10 16:52:27 +0530

    changes in curve line

--- components/AboutUs/Navbar.tsx
+++ components/AboutUs/Navbar.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <>
-      <div className="h-6 bg-[#312783] text-white text-[12px] flex items-center justify-between px-8">
-        <span>
-          Speak to our travel advisor (773) 093-8067 | Open 10am-7pm CST
-        </span>
+      <div className="flex items-center justify-between bg-[#2E2787] px-14 py-2 text-[11px] text-white">
+        <p>
+          Speak to our travel advisor (773) 983-8067 | open 10am-7pm CST
+        </p>
 
-        <div className="flex gap-4">
-          <span>FAQs</span>
-          <span>Contact Us</span>
+        <div className="flex items-center gap-6">
+          <button>FAQs</button>
+          <button>Contact Us</button>
         </div>
       </div>
 
@@ -23,17 +23,14 @@ export default function Navbar() {
           priority
         />
 
-        <ul
-          className="flex gap-10 text-[18px] font-normal tracking-[0.05em] text-[#1A1A1A]"
-          style={{ fontFamily: "Nohemi" }}
-        >
-          <li>About</li>
-          <li>Group Journeys</li>
-          <li>Private Journeys</li>
-          <li>Tailor-made Journeys</li>
-          <li>Destinations</li>
-          <li>Offers</li>
-        </ul>
+         <div className="flex items-center gap-9 text-[14px] font-medium text-[#1E1E1E]">
+            <button>About</button>
+            <button>Group Journeys</button>
+            <button>Private Journeys</button>
+            <button>Tailor-made Journeys</button>
+            <button>Destinations</button>
+            <button>Offers</button>
+          </div>
       </div>
     </>
   );
--- components/AboutUs/SearchBar.tsx
+++ components/AboutUs/SearchBar.tsx
@@ -1,6 +1,6 @@
 export default function SearchBar() {
   return (
-    <div className="flex justify-center gap-3 py-3">
+    <div className="w-[92%] mx-auto flex justify-center gap-3 py-3">
       <select className="w-[250px] h-[40px] border border-[#d0d0d0] px-3 text-[13px] text-gray-500">
         <option>How do you want to travel?</option>
       </select>
@@ -13,7 +13,7 @@ export default function SearchBar() {
         <option>Where do you want to travel?</option>
       </select>
 
-      <button className="h-[40px] px-8 rounded-full bg-[#312783] text-white font-medium">
+      <button className="h-[40px] w-[160px] rounded-full bg-[#2E348D] text-white">
         Find Your Journey
       </button>
     </div>
