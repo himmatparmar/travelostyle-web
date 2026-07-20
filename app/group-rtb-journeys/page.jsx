b0e0e7c946e6e6cf4f356ffe9b4327b47639b196
alka-saiesha <alka.kumari@saiesha.us>
2026-07-20 12:48:54 +0530

    Created reusable dynamic components: GroupRevelationsSection and TravelJourneyCard.

--- app/group-rtb-journeys/page.jsx
+++ app/group-rtb-journeys/page.jsx
@@ -1,22 +1,12 @@
 import Footer from "../../components/Footer";
-import GoodCompanyJourney from "../../components/GroupRtbJourneys/GoodCompanyJourney";
-import ChoosePopularGroupJourney from "../../components/GroupRtbJourneys/ChoosePopularGroupJourney";
-import GroupRevelationsSection from "../../components/GroupRtbJourneys/GroupRevelationsSection";
+import Index from "../../components/GroupRtbJourneys/Index";
 import SearchBar from "../../components/HomePage/FindYourJourney/SearchBar";
-import TravelOStylePromo from "../../components/GroupRtbJourneys/TravelOStylePromo";
-import BookingSteps from "../../components/GroupRtbJourneys/BookingSteps";
-import AdvisorCallout from "../../components/GroupRtbJourneys/AdvisorCallout";
 
 export default function Page() {
   return (
     <>
        <SearchBar/>
-      <GoodCompanyJourney/>
-       <GroupRevelationsSection/>
-       <ChoosePopularGroupJourney/>
-       <TravelOStylePromo/>
-       <BookingSteps/>
-       <AdvisorCallout/>
+     <Index/>
       <Footer />
     </>
   );
